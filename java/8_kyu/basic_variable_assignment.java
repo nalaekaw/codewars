13a90b3aaa19f9721fe28a5585087964c99727a8
Andrey Lavrentiev <aaa.lavrentev@gmail.com>
2021-10-19 12:56:44 +1000

    Create basic_variable_assignment.java

---
+++ java/8_kyu/basic_variable_assignment.java
@@ -0,0 +1,30 @@
+// Instructions
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+  
+//   public class Kata {
+//     public static String a == "code";
+//     public static String b == "wa.rs";
+//     public static String name == a + b;
+// }
+
+// Solution
+
+public class Kata {
+    public static String a = "code";
+    public static String b = "wa.rs";
+    public static String name = a + b;
+}
+
+
+// Tests
+
+// import org.junit.Test;
+// import static org.junit.Assert.assertEquals;
+// import org.junit.runners.JUnit4;
+
+// public class Tests {
+//     @Test
+//     public void correctValue() {
+//         assertEquals("codewa.rs", Kata.name);
+//     }
+// }
