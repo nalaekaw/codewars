d80a8d37bf8100bb79485af16210ca57a1bcabbf
Andrey Lavrentiev <aaa.lavrentev@gmail.com>
2021-10-20 10:59:43 +1000

    Create variable_assignment_debug.py

---
+++ python/8_kyu/variable_assignment_debug.py
@@ -0,0 +1,31 @@
+# Instructions
+
+# Variable assignment
+# Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+# a == "dev"
+# b == "Lab"
+# name ==  a + b
+
+
+# Solution
+
+a = "dev"
+b = "Lab"
+name = a + b
+
+
+# Sample Tests
+
+# import codewars_test as test
+# from solution import a, b, name
+
+# @test.describe('variable assignment')
+# def fixed_tests():
+#     @test.it('should set variable a')
+#     def variable_a():
+#         test.assert_equals(a, 'dev')
+#     @test.it('should set variable b')
+#     def variable_b():   
+#         test.assert_equals(b, 'Lab')
+    
