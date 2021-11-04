bf317ab28daa202e88948e0af4f1c5f0720af4b5
Andrey Lavrentiev <aaa.lavrentev@gmail.com>
2021-11-05 00:52:41 +1000

    Update are_you_playing_banjo.R

--- r/8_kyu/are_you_playing_banjo/are_you_playing_banjo.R
+++ r/8_kyu/are_you_playing_banjo/are_you_playing_banjo.R
@@ -1,8 +1,3 @@
 are_you_playing_banjo <- function(name){
-if(startsWith(name, 'r')||startsWith(name,'R')){
-    result <- paste(name, "plays banjo")
-}else {
-    result <- paste(name, "does not play banjo")
-}
-    return (result)
+  paste(name, ifelse(startsWith(tolower(name), "r"), "plays", "does not play"), "banjo")
 }
